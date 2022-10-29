d2d3b72b9dc57b2c6ccc26c135da4069b96c1e2f
derrykid@github.com <tzuderry@gmail.com>
2022-10-29 10:59:04 +0100

    implement vocabulary class

--- src/main/java/jp/dictionary/Vocabulary.java
+++ src/main/java/jp/dictionary/Vocabulary.java
@@ -2,7 +2,29 @@ package jp.dictionary;
 
 public class Vocabulary implements Word {
 
+    private final String english;
+    private final String chinese;
+
     public Vocabulary(String english, String chinese) {
+
+        this.english = english;
+        this.chinese = chinese;
+
     }
 
+    @Override
+    public String getEnglish() {
+        return english;
+    }
+
+    @Override
+    public String getChinese() {
+        return chinese;
+    }
+
+    @Override
+    public String toString() {
+        // "water": 意思是 n.水,水域,领海,困境;v.灌溉,流泪，流口水
+        return this.english + ": 意思是 " + this.chinese;
+    }
 }
