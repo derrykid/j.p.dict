376a54318c512b03ad68f1bd6513557d7b3f3014
charlie0836 <109490682+charlie0836@users.noreply.github.com>
2022-10-29 19:32:23 +0800

    Update Word.java

--- src/main/java/jp/dictionary/Word.java
+++ src/main/java/jp/dictionary/Word.java
@@ -1,12 +1,16 @@
 package jp.dictionary;
 
 public interface Word {
+    
+    Vocabulary(getEnglish(),getChinese()){
+        this.english=getEnglish();
+        this.chinese=getChinese();
+    }
+    //String getEnglish();
 
-    String getEnglish();
-
-    String getChinese();
+    //String getChinese();
 
     String toString();
-
+    System.out.println(toString());
 
 }
