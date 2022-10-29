c17ea285735380abeb430621f2eeec46595299e2
charlie0836 <109490682+charlie0836@users.noreply.github.com>
2022-10-29 23:55:39 +0800

    Update word.java

--- src/main/java/jp/dictionary/word.java
+++ src/main/java/jp/dictionary/word.java
@@ -4,7 +4,7 @@ public class Word{
 
         Map<String, String> map = new HashMap<String, String>();
     while(english==in.nextLine()){
-        chinese=in.nextLine()
+        chinese=in.nextLine();
         map.put(getEnglish(),getChinese());//map.put("dog", "type of animal");
         System.out.println(toString());//System.out.println(map.get("dog"));
     }
